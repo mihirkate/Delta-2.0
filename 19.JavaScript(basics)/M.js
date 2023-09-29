278c9aee92f0c8be3778e3a6166e829ffb4ccef0
Mihir Kate <mihirkate13@gmail.com>
2023-09-29 14:28:12 +0530

    added App.js

--- 19.JavaScript(basics)/M.js
+++
@@ -1,8 +0,0 @@
-let a;
-let c=0;
-let b=null;
-let d=0;
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
---
+++ 19.JavaScript(basics)/app.js
@@ -0,0 +1,15 @@
+let a;
+let c=0;
+let b=null;
+let d=0;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log("Mihir Kate ")
+
+
+let price1=0;
+let price2=19;
+let output=`The output is :${price1+price2} Rupees`
+console.log(output);
