74e80297456a404fef4d84193bfa1fc282750745
Mihir Kate <mihirkate13@gmail.com>
2023-10-02 17:07:11 +0530

    Basics

--- 19.JavaScript(basics)/Arrays.js
+++ 19.JavaScript(basics)/Arrays.js
@@ -1,6 +1,6 @@
 let arr = ["Mihir ", "Kate"];
 console.log(arr);
-let nums = [2, 3, 4, 6, 78];
+let nums = [2, "Mihir", 3, 4, 6, 78];
 console.log(nums[4]);
 let m = [];
-console.log(m);
+console.log(nums[1][4]);
