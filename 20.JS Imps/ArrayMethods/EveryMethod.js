7c6450594030cb88f184cbf9953a1a89379bc5d7
Mihir Kate <mihirkate13@gmail.com>
2023-10-12 18:08:38 +0530

    added Max Array

--- 20.JS Imps/ArrayMethods/EveryMethod.js
+++ 20.JS Imps/ArrayMethods/EveryMethod.js
@@ -8,9 +8,20 @@ console.log(
 Accumalator matlab last time jo function ne return ki thi 
 vo value firse return hogi but current addition ke sath 
 */
-let nums = [1, 2, 3, 4, 5, 6, 8];
-let final = nums.reduce((res, el) => {
+let nums = [1, 2, 7, 9, 5, 6, 8];
+/*let final = nums.reduce((res, el) => {
   console.log(res * el);
   return res * el;
 });
-console.log(final);
+console.log(final); */
+
+// max in array
+let maxNo = nums.reduce((res, el) => {
+  if (el > res) {
+    console.log(" ", el);
+    return el;
+  } else {
+    return res;
+  }
+});
+console.log(maxNo);
