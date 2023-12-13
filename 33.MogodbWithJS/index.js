c5a31a5526d058fef8e263132e60230010de1df0
Mihir Kate <mihirkate13@gmail.com>
2023-12-13 21:23:51 +0530

    WEB

---
+++ 33.MogodbWithJS/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+main()
+  .then((res) => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+const User = mongoose.model("User", userSchema);
+const user2 = new User({
+  name: "eve",
+  email: "eve@yahoo.com",
+  age: 19,
+});
+user2
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
